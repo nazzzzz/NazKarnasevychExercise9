073db6574cc02296c1a3ef69c41c5f5a64fc4201
nazzzzz <nk1721@nyu.edu>
2016-11-28 18:08:57 -0500

    Add files via upload

--- NazKarnasevychExercise9/sketch.js
+++ NazKarnasevychExercise9/sketch.js
@@ -1,9 +1,9 @@
 var summonerName = "nazzz";
-var summonerID;
+var summonerID, season;
 var apiKey = "RGAPI-499ea6d0-0050-4e59-a77a-b554e7dd2034";
-var json_lvl, json_masteries;
+var json_lvl;
 
-var masteryPageNumber, runePageNumber;
+var masteryPageNumber, runePageNumber, wins, champPoints, champID;
 
 
 function setup() {
@@ -18,32 +18,84 @@ function preload() {
 function draw() {
     text("Your account at a glance", 300, 50);
 
-    text("Mastery Page Number:", 150, 150);
+    text("Ranked wins in current season:", 150, 150);
 
-    text("Rune Page Number:", 150, 180);
+    text("Number of mastery pages:", 150, 350);
+
+    text("Number of rune pages:", 150, 380);
     summonerLookUp();
     noLoop();
 }
 
-function masteryLookup(json_masteries) {
-
-
+function masteriesLookup(json_masteries) {
+	//get number of mastery pages
     masteryPageNumber = json_masteries[summonerID].pages.length;
-    console.log(masteryPageNumber);
-    text(masteryPageNumber, 300, 150);
+
+    //console.log(masteryPageNumber);
+
+    //output text
+    text(masteryPageNumber, 350, 350);
 }
 
 function runeLookup(json_runes){
+	//get number of rune pages
 	runePageNumber = json_runes[summonerID].pages.length;
-	console.log(runePageNumber);
-	text(runePageNumber, 300, 180);
+
+	//console.log(runePageNumber);
+	
+	//output text
+	text(runePageNumber, 350, 380);
+}
+
+function getRankedWins(json_ranked){
+	//get wins for solo ranked
+	wins = json_ranked.playerStatSummaries[4].wins;
+
+	//update with wins from flex queue ranked
+	wins+= json_ranked.playerStatSummaries[10].wins;
+
+	//console.log(wins);
+
+	//output text
+	text(wins, 350, 150)
+}
+
+function getHighestMastery(json_mastery){
+	champPoints = json_mastery[0].championPoints;
+	champID = json_mastery[0].championID;
+	console.log(champPoints);
+	console.log(champID);
+
+	loadJSON("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" +champID + "?api_key=" + apiKey, getChampName);
+
+}
+
+function getChampName(json_champ){
+	champName = json_champ.name;
+
 }
 
 function summonerLookUp() {
+	//get summoner id for later use
     summonerID = json_lvl[summonerName].id;
     console.log(summonerID);
 
-    loadJSON("https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + summonerID + '/masteries?api_key=' + apiKey, masteryLookup);
+    //load JSONS using the summoner ID
+    //multiple calls are needed because of different APIs
+
+    //load json for masteries
+    loadJSON("https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + summonerID + '/masteries?api_key=' + apiKey, masteriesLookup);
+
+    //load json for runes
     loadJSON("https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + summonerID + '/runes?api_key=' + apiKey, runeLookup);
+
+    //load json for stats
+	loadJSON("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + summonerID + '/summary?season=SEASON2016&api_key=' + apiKey, getRankedWins);
+
+	//load json for champ mastery
+	
+	//loadJSON("https://na.api.pvp.net/championmastery/location/NA1/player/" +summonerID + "/topchampions?count=1&api_key=" + apiKey, getHighestMastery);
+
+	loadJSON("https://na.api.pvp.net/championmastery/location/NA1/player/29293511/topchampions?count=1&api_key=RGAPI-499ea6d0-0050-4e59-a77a-b554e7dd2034", getHighestMastery);
 
 }
